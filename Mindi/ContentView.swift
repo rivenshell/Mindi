02d827bcca81cdfbaef16226b665a287b9c5dcb2
rivenshell <rivenshell111@gmail.com>
2025-11-08 20:22:23 -0500

    Initial Commit

---
+++ Mindi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mindi
+//
+//  Created by Riv Sal on 11/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mindi/MindiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindiApp.swift
+//  Mindi
+//
+//  Created by Riv Sal on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MindiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
